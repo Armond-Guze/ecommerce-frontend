fae61398acbc1da69b54ca9b0ea4a6a00ebd081a
Armond-Guze <Armond.Guze@yahoo.com>
2024-04-19 22:01:34 -0400

    navbar component completed

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import Navbar from "./Components/Navbar"
+export default function App() {
+  return (
+  < Navbar/>
+  )
+}
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { RiMenuAddFill } from "react-icons/ri";
+
+function Navbar() {
+  const [ nav, setNav ] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+  return (
+    <div className="text-white flex justify-between items-center h-24 max-w-91240px mx-auto px-4">
+      <h1 className="w-full text-3xl font-bold text-[#00df98] ">REACT.</h1>
+      <ul className="flex font-bold hidden">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+      <div onClick={handleNav}>
+        {!nav ? <RiMenuAddFill size={25}/> : <RiMenuAddFill size={25} />}
+      </div>
+      <div className="fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300]">
+        <h1 className="w-full text-3xl font-bold text-[#00df98] m-4">REACT.</h1>
+
+        <ul className="uppercase p-4">
+          <li className="p-4 border-b border-gray-600">Home</li>
+          <li className="p-4 border-b border-gray-600">Company</li>
+          <li className="p-4 border-b border-gray-600">Resources</li>
+          <li className="p-4 border-b border-gray-600">About</li>
+          <li className="p-4">Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
