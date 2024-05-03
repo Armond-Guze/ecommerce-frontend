ddb62583f9f1704d5a578908fed1271ab4ccd054
Armond-Guze <Armond.Guze@yahoo.com>
2024-05-03 16:44:37 -0400

    edited cards

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -21,7 +21,7 @@ function Cards() {
         </div>
         <div className='w-full shadow-xl flex bg-gray-100 flex-col py-4 my-8 md:my-0 rounded-lg hover:scale-105 duration-300'>
             <img className='w-20 mx-auto mt-[-3rem] bg-white bg-transparent' src={Double} alt="" />
-            <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
+            <h2 className='text-2xl font-bold text-center py-8'>Two Users</h2>
             <p className='text-center text-4xl font-bold '>$149</p>
             <div className='text-center font-medium'>
                 <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
@@ -32,7 +32,7 @@ function Cards() {
         </div>
         <div className='w-full shadow-xl flex flex-col py-4 my-4 rounded-lg hover:scale-105 duration-300'>
             <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="" />
-            <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
+            <h2 className='text-2xl font-bold text-center py-8'>Three Users</h2>
             <p className='text-center text-4xl font-bold '>$149</p>
             <div className='text-center font-medium'>
                 <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
