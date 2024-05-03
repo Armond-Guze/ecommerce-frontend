6a5dde99d4e37d6f11e90c8ac0af0969be79a102
Armond-Guze <Armond.Guze@yahoo.com>
2024-05-03 16:41:35 -0400

    netlify errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import Analytics from "./Components/Analytics";
+import Newsletter from "./Components/Newsletter";
+import Cards from "./Components/Cards";
+import Footer from "./Components/Footer";
 export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
   };
   return (
     <div className="text-white flex justify-between items-center h-24 max-w-91240px mx-auto px-4">
-      <h1 className="w-full text-3xl font-bold text-[#00df98] ">REACT.</h1>
+      <h1 className="w-full text-3xl font-bold text-[#00df98] pl-4 ">REACT.</h1>
       <ul className="hidden md:flex cursor-pointer font-bold">
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>
@@ -18,10 +18,10 @@ function Navbar() {
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
-      <div onClick={handleNav} className="block md:hidden">
-        {!nav ? <AiOutlineClose size={25} /> : <RiMenuAddFill size={25} />}
+      <div onClick={handleNav} className="block md:hidden cursor-pointer">
+        {nav ? <AiOutlineClose size={25} /> : <RiMenuAddFill size={25}/>}
       </div>
-      <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[100%]'}>
+      <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[100%]'}>
         <h1 className="w-full text-3xl font-bold text-[#00df98] m-4">REACT.</h1>
 
         <ul className="uppercase p-4">
